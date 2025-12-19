0104571ad16a0654d5d4d81fc26f1dc7cd02de7f
dryrain22 <dryrainee@gmail.com>
2025-12-19 17:39:20 +0900

    feat: add api response

---
+++ src/main/java/org/onlix/catalog/core/utils/LogUtils.java
@@ -0,0 +1,25 @@
+package org.onlix.catalog.core.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.onlix.catalog.core.exception.BaseErrorCode;
+
+
+@Slf4j
+public class LogUtils {
+
+    public static void warn(BaseErrorCode errorCode) {
+        log.warn("[{}] {} - {}",
+                errorCode.getStatusCode(),
+                errorCode.name(),
+                errorCode.getMessage());
+    }
+
+    // Error는 심각한 상황이라, stack trace도 출력
+    public static void error(BaseErrorCode errorCode, Exception ex) {
+        log.error("[{}] {} - {}",
+                errorCode.getStatusCode(),
+                errorCode.name(),
+                errorCode.getMessage(),
+                ex);
+    }
+}
